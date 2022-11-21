ea51320b3e3742566aae731f16b38980984c5d26
DESKTOP-6M69AE9\admin <nmk02@naver.com>
2022-11-21 16:40:03 +0900

    타입 및 함수

---
+++ ch03/ex5.py
@@ -0,0 +1,18 @@
+# 대소문자 구분
+# 자바와 선언 문법은 같음
+
+boolVar = False
+intVar = 100
+floatVar = 123.45
+strVar1 = "반갑다 월드컵"
+
+# 서로 다른 타입도 한 변수로 사용할 수 있다.
+strVar2 = '반갑다 월드컵2'
+print(strVar2)
+strVar2 = True
+print(strVar2)
+
+print(type(intVar))     # 결과값 : <class 'int'>
+print(type(strVar1))    # 결과값 : <class 'str'>
+
+
